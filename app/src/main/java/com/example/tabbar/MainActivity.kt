efade8c5834e1b155bd0bf209bc88240d65fab47
Yaroslav Zavyalov <yozavyalov@avito.ru>
2024-04-29 22:21:24 +0200

    Created simple TabBar

---
+++ app/src/main/java/com/example/tabbar/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tabbar
+
+import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                R.id.bottom_reels -> {
+                    replaceFragment(ReelsFragment())
+                    true
+                }
+                R.id.bottom_add -> {
+                    Toast.makeText(this, "Upload image and video", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+}
